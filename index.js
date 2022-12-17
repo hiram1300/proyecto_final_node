9db8d94861a53610f13ddf3abd717505d248cdab
Hiram Guevara <hguevara19@alumnos.uaq.mx>
2022-12-16 23:25:04 -0600

    add database and dependencies

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
+//dependencies
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res, next) => {
-    res.send('Welcome')
-});
+//routes
+const employeeR = require('./routes/employees');
+const user = require('./routes/user');
 
-app.listen(3000, () => {
-    console.log("Server is running")
+//middleware
+const auth = require('./middleware/auth');
+const notFound = require('./middleware/notFound');
+const index = require('./middleware/index');
+const cors = require('./middleware/cors')
+
+app.use(cors);
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", index);
+
+app.use("/user", user);
+app.use(auth);
+app.use("/employees", employeeR);
+
+app.use(notFound);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is Running...");
 });
