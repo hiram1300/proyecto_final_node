9b062d7337a993d77ef2780f0dab3c29303dff65
Hiram Guevara <hguevara19@alumnos.uaq.mx>
2022-12-16 23:13:18 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res, next) => {
+    res.send('Welcome')
+});
+
+app.listen(3000, () => {
+    console.log("Server is running")
+});
